9bd75e056b9146bc58e32d6f09659b81f0cdbfbc
Ikkurthi Nanda Gopal <115791364+nandagopal04@users.noreply.github.com>
2023-09-25 14:11:04 +0530

    styles.css

---
+++ styles.cs
@@ -0,0 +1,26 @@
+body {
+    font-family: Arial, sans-serif;
+    background: linear-gradient(to right, #ffffff, #000000);
+    margin: 0;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.container {
+    text-align: center;
+    background: rgba(255, 255, 255, 0.8);
+    padding: 20px;
+    border-radius: 5px;
+}
+
+.color-container {
+    margin: 10px 0;
+}
+
+button {
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+}
